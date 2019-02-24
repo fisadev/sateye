045f1b9250761b2b089263e73b9f9058712f3103
Juan Pedro Fisanotti <fisadev@gmail.com>
2019-02-24 18:36:57 -0300

    Logic that will be shared through most of the web server (api responses and alert kinds for now)

---
+++ sateye/core.py
@@ -0,0 +1,30 @@
+from enum import Enum
+
+from flask import jsonify
+
+
+class Alert(Enum):
+    """
+    Types of alerts we display to users.
+    """
+    INFO = "info"
+    ERROR = "error"
+    WARNING = "warning"
+
+
+def api_response(ok=True, data=None, alerts=None):
+    """
+    Build a json api result in the format expected by the client side.
+    """
+    alerts = alerts or []
+
+    # shortcut to be able to call api_response with just one alert
+    if alerts and isinstance(alerts[0], Alert):
+        alerts = [alerts]
+
+    return jsonify({
+        "ok": ok,
+        "data": data or {},
+        "alerts": [{"type": alert_type.value, "message": alert_message}
+                   for alert_type, alert_message in alerts]
+    })
