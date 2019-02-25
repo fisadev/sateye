73430abcd835c43e7c866b9d51ec3139756da13f
Juan Pedro Fisanotti <fisadev@gmail.com>
2019-02-25 01:51:05 -0300

    Scaffolding for satellite prediction logic, so the frontend doesn't crash all the time

--- sateye/sateye.py
+++ sateye/sateye.py
@@ -4,9 +4,11 @@ from pathlib import Path
 
 from flask import Flask, render_template, request
 from jinja2 import Markup
+from dateutil.parser import parse as parse_date
 
 from sateye import storage
 from sateye.core import Alert, api_response
+from sateye.satellites import Satellite
 
 
 app = Flask(__name__)
@@ -56,4 +58,17 @@ def api_user_data():
         ok=True,
         payload=user_data,
         alerts=alerts,
+    )
+
+@app.route("/api/satellite/<int:satellite_id>/path_prediction/")
+def api_sate_path_prediction(satellite_id):
+    start = parse_date(request.args.get("start"))
+    duration = int(request.args.get("duration"))
+
+    sate = Satellite.get_by_id(satellite_id)
+    path_info = sate.predict_path(start, duration)
+
+    return api_response(
+        ok=True,
+        payload=path_info,
     )
--- sateye/static/js/satellites.js
+++ sateye/static/js/satellites.js
@@ -3,6 +3,7 @@ sateye.satellites = {
 
     initialize: function() {
         sateye.satellites.active.push({
+            id: "42",
             name: "milanesat",
             tle: "newsat3 42760U\n1 42760U 17034C   18035.48142696  .00000261  00000-0  29808-4 0  9991\n2 42760  43.0171  92.1179 0007969 353.6733 135.8363 15.09016581 35404",
             pathInfo: null,
@@ -15,9 +16,16 @@ sateye.satellites = {
             getMorePredictions: function(fromTime, secondsAhead) {
                 // get more predictions, to fill X map seconds starting at a given time
                 console.log("Requesting predictions for satellite " + this.name);
-                $.ajax({url: "/predictions/", cache: false})
-                 .done(sateye.satellites.onPredictionsReceived)
-                 .fail(sateye.satellites.onPredictionsError);
+                $.ajax({
+                    url: "/api/satellite/" + this.id + "/path_prediction/", 
+                    cache: false,
+                    data: {
+                        start: fromTime.toString(),
+                        duration: secondsAhead,
+                    }
+                })
+                .done(sateye.satellites.onPredictionsReceived)
+                .fail(sateye.satellites.onPredictionsError);
             },
 
             onPredictionsReceived: function(data) {
