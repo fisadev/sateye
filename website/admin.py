c27e10d79a8fc35df82bf2edad4c681d6313cafb
Juan Pedro Fisanotti <fisadev@gmail.com>
2019-02-28 01:05:43 -0300

    Basic satellite and tle models, with admins

--- website/admin.py
+++ website/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
 
-# Register your models here.
+from website.models import Satellite, TLE
+
+
+class SatelliteAdmin(admin.ModelAdmin):
+    """
+    Customization for the Satellite model in the admin site.
+    """
+    list_display = 'name', 'owner'
+
+
+class TLEAdmin(admin.ModelAdmin):
+    """
+    Customization for the TLE model in the admin site.
+    """
+    list_display = 'satellite', 'at'
+    list_filter = 'satellite', 'at'
+    date_hierarchy = 'at'
+
+
+admin.site.register(Satellite, SatelliteAdmin)
+admin.site.register(TLE, TLEAdmin)
--- website/models.py
+++ website/models.py
@@ -1,3 +1,27 @@
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+
+class Satellite(models.Model):
+    """
+    A specific satellite in orbit, that Sateye can display and track.
+    """
+    name = models.CharField(max_length=100)
+    description = models.TextField(null=True, blank=True)
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, null=True,
+                              blank=True, related_name='satellites')
+
+    def __str__(self):
+        return 'Satellite: {}'.format(self.name)
+
+
+class TLE(models.Model):
+    """
+    Orbital information from a specific satellite at a specific moment in time.
+    """
+    satellite = models.ForeignKey(Satellite, on_delete=models.CASCADE, related_name='tles')
+    at = models.DateTimeField()
+    lines = models.TextField()  # the actual two line element
+
+    def __str__(self):
+        return 'TLE at {}'.format(self.at)
