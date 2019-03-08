e06751ebe73282b4b59b9b4768a69491222649c3
Juan Pedro Fisanotti <fisadev@gmail.com>
2019-03-07 21:20:33 -0300

    Server api will now receive cleaner params

--- website/views.py
+++ website/views.py
@@ -21,15 +21,13 @@ def api_predict_path(request, satellite_id):
     Get predictions for a satellite.
     """
     satellite = Satellite.objects.get(pk=satellite_id)
-    center = parse_date(request.GET['center'])
-    duration = int(request.GET['duration'])
+    start_date = parse_date(request.GET['start_date'])
+    end_date = int(request.GET['end_date'])
     steps = int(request.GET['steps'])
 
-    center = center.replace(microsecond=0)
-    start_date = center - timedelta(seconds=duration)
-    end_date = center + timedelta(seconds=duration)
+    duration = (end_date - start_date).total_seconds()
+    step_seconds = duration / steps
 
-    step_seconds = duration / (steps * 2)
     positions = satellite.predict_path(start_date, end_date, step_seconds)
     cesium_data = cesium_utils.generate_path_data(satellite, start_date, end_date, positions)
 
