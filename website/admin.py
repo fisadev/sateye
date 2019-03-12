d1137f2958749753040023d4e52151aa89b76366
Juan Pedro Fisanotti <fisadev@gmail.com>
2019-03-11 23:36:04 -0300

    Admin for the two new models

--- website/admin.py
+++ website/admin.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
 
-from website.models import Satellite, TLE, Location
+from website import models
 
 
-@admin.register(Satellite)
+@admin.register(models.Satellite)
 class SatelliteAdmin(admin.ModelAdmin):
     """
     Customization for the Satellite model in the admin site.
@@ -11,7 +11,7 @@ class SatelliteAdmin(admin.ModelAdmin):
     list_display = ('name', 'norad_id', 'owner')
 
 
-@admin.register(TLE)
+@admin.register(models.TLE)
 class TLEAdmin(admin.ModelAdmin):
     """
     Customization for the TLE model in the admin site.
@@ -21,7 +21,23 @@ class TLEAdmin(admin.ModelAdmin):
     date_hierarchy = 'at'
 
 
-@admin.register(Location)
+@admin.register(models.Dashboard)
+class DashboardAdmin(admin.ModelAdmin):
+    """
+    Customizations for the Dashboard model in the admin site.
+    """
+    list_display = ('name', 'owner')
+
+
+@admin.register(models.DashboardSatelliteConfig)
+class DashboardSatelliteConfigAdmin(admin.ModelAdmin):
+    """
+    Customizations for the DashboardSatelliteConfigAdmin model in the admin site.
+    """
+    list_display = ('dashboard', 'satellite')
+
+
+@admin.register(models.Location)
 class LocationAdmin(admin.ModelAdmin):
     """
     Customizations for the Locations model in the admin site.
