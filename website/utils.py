417ae94cbf01466d2f5f0b49fa82a2974f191af9
Agustin Scaramuzza <agustinscaramuzza@gmail.com>
2019-04-12 23:05:07 -0300

    Update orbit-predictor, remove custom get_predictor function

--- website/utils.py
+++ website/utils.py
@@ -4,30 +4,8 @@ import pytz
 
 from django.utils.timezone import make_naive, is_aware
 
-from sgp4.earth_gravity import wgs84
-from sgp4.io import twoline2rv
-from orbit_predictor.accuratepredictor import HighAccuracyTLEPredictor
-from orbit_predictor.predictors import TLEPredictor
-from orbit_predictor.sources import MemoryTLESource
-
 
 Pass = namedtuple('Pass', 'satellite location aos tca los tca_elevation sun_elevation')
-
-
-def get_predictor_from_tle_lines(tle_lines, precise=False):
-    """
-    This will be in orbit_predictor 1.8.7 when it gets released to pypi.
-    """
-    source = MemoryTLESource()
-    sgp4_sat = twoline2rv(tle_lines[0], tle_lines[1], wgs84)
-    source.add_tle(sgp4_sat.satnum, tuple(tle_lines), sgp4_sat.epoch)
-
-    if precise:
-        predictor = HighAccuracyTLEPredictor(sgp4_sat.satnum, source)
-    else:
-        predictor = TLEPredictor(sgp4_sat.satnum, source)
-
-    return predictor
 
 
 def ensure_naive(date):
