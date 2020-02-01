a41b7ce874881179d6a07ea2f3fc60bdf4bc13ec
Juan Pedro Fisanotti <fisadev@gmail.com>
2020-02-01 01:07:34 -0300

    Updated the urls for the simplified api

--- website/api/urls.py
+++ website/api/urls.py
@@ -4,17 +4,11 @@ from rest_framework import routers
 from website.api import views
 
 router = routers.SimpleRouter()
-router.register(r'satellites/(?P<satellite_id>\d+)/tles', views.TLEViewSet, basename='TLE')
 router.register(r'satellites', views.SatelliteViewSet, basename='Satellite')
-router.register(r'dashboards/(?P<dashboard_id>\d+)/satellite_configs',
-                views.DashboardSatelliteConfigViewSet, basename='DashboardSatelliteConfig')
-router.register(r'dashboards/(?P<dashboard_id>\d+)/location_configs',
-                views.DashboardLocationConfigViewSet, basename='DashboardLocationConfig')
 router.register(r'dashboards', views.DashboardViewSet, basename='Dashboard')
-router.register(r'locations', views.LocationViewSet, basename='Location')
 
 urlpatterns = router.urls
 urlpatterns += [
-    path('satellites/<int:satellite_id>/predict_path/', views.predict_path),
+    path('predict_path/', views.predict_path),
     path('predict_passes/', views.predict_passes),
 ]
