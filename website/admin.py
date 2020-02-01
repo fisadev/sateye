bf09e74718cd358d1a7871254653d5cc0d3f793e
Juan Pedro Fisanotti <fisadev@gmail.com>
2020-02-01 01:05:29 -0300

    Updated admin after model changes

--- website/admin.py
+++ website/admin.py
@@ -8,25 +8,7 @@ class SatelliteAdmin(admin.ModelAdmin):
     """
     Customization for the Satellite model in the admin site.
     """
-    list_display = ('name', 'norad_id', 'owner')
-
-
-@admin.register(models.TLE)
-class TLEAdmin(admin.ModelAdmin):
-    """
-    Customization for the TLE model in the admin site.
-    """
-    list_display = ('satellite', 'at')
-    list_filter = ('satellite', 'at')
-    date_hierarchy = 'at'
-
-
-@admin.register(models.Location)
-class LocationAdmin(admin.ModelAdmin):
-    """
-    Customizations for the Locations model in the admin site.
-    """
-    list_display = ('name', 'owner', 'latitude', 'longitude', 'elevation')
+    list_display = ('name', 'norad_id', 'tle_date')
 
 
 @admin.register(models.Dashboard)
@@ -35,19 +17,3 @@ class DashboardAdmin(admin.ModelAdmin):
     Customizations for the Dashboard model in the admin site.
     """
     list_display = ('name', 'owner')
-
-
-@admin.register(models.DashboardSatelliteConfig)
-class DashboardSatelliteConfigAdmin(admin.ModelAdmin):
-    """
-    Customizations for the DashboardSatelliteConfigAdmin model in the admin site.
-    """
-    list_display = ('dashboard', 'satellite')
-
-
-@admin.register(models.DashboardLocationConfig)
-class DashboardLocationConfigAdmin(admin.ModelAdmin):
-    """
-    Customizations for the DashboardLocationConfigAdmin model in the admin site.
-    """
-    list_display = ('dashboard', 'location')
