ca18c0a1c8cf27d43e0759ecef94357b7ce2d2c2
Juan Pedro Fisanotti <fisadev@gmail.com>
2020-02-01 01:27:23 -0300

    Use a large textfield for tles

--- website/admin.py
+++ website/admin.py
@@ -1,3 +1,4 @@
+from django import forms
 from django.contrib import admin
 
 from website import models
@@ -9,6 +10,14 @@ class SatelliteAdmin(admin.ModelAdmin):
     Customization for the Satellite model in the admin site.
     """
     list_display = ('name', 'norad_id', 'tle_date')
+
+    def formfield_for_dbfield(self, db_field, **kwargs):
+        formfield = super(SatelliteAdmin, self).formfield_for_dbfield(db_field, **kwargs)
+        if db_field.name == 'tle':
+            formfield.widget = forms.Textarea(attrs=formfield.widget.attrs)
+            formfield.widget.attrs['class'] = 'vLargeTextField'
+
+        return formfield
 
 
 @admin.register(models.Dashboard)
