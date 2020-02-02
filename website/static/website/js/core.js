837fb4b6ae2b41eb704f60bafd366f95799643ba
Juan Pedro Fisanotti <fisadev@gmail.com>
2020-02-02 01:26:26 -0300

    Function to be able to generate uuids

--- website/static/website/js/core.js
+++ website/static/website/js/core.js
@@ -88,5 +88,11 @@ var sateye = function() {
         return number.toFixed(2);
     }
 
+    self.uuidv4 = function() {
+        return ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g, c =>
+            (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
+        );
+    }
+
     return self;
 }();
