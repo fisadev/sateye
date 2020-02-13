318cead2d5d815c0a13a6302def7fe03f7d980e1
Juan Pedro Fisanotti <fisadev@gmail.com>
2020-02-13 19:14:13 -0300

    On save and read, dashboards populate and clean fields that must come from the server db

--- website/api/serializers.py
+++ website/api/serializers.py
@@ -1,6 +1,9 @@
+import json
+
 from rest_framework import serializers
 
-from website import models
+from website.models import Dashboard, Satellite
+
 
 
 class SatelliteSerializer(serializers.ModelSerializer):
@@ -8,14 +11,48 @@ class SatelliteSerializer(serializers.ModelSerializer):
     Serializer for the satellite api.
     """
     class Meta:
-        model = models.Satellite
+        model = Satellite
         fields = ['id', 'name', 'norad_id', 'description', 'tle', 'tle_date']
+
+
+class DashboardConfigField(serializers.Field):
+    """
+    Dashboard config can have information that is read from the database instead of the config,
+    on get, but never stored.
+    """
+    def to_representation(self, value):
+        # TODO handle errors?
+        config = json.loads(value)
+
+        # populate all fields of satellites that come form the server db
+        for satellite_data in config['satellites']:
+            if satellite_data['from_db']:
+                satellite = Satellite.objects.get(pk=satellite_data['id'])
+                serializer = SatelliteSerializer(satellite)
+                satellite_data.update(serializer.data)
+
+        return json.dumps(config)
+
+    def to_internal_value(self, data):
+        # TODO handle errors?
+        config = json.loads(data)
+
+        # remove all fields from satellites that come form the server db
+        for satellite_data in config['satellites']:
+            if satellite_data['from_db']:
+                for key in list(satellite_data.keys()):
+                    if key not in ('id', 'from_db'):
+                        del satellite_data[key]
+
+        return json.dumps(config)
 
 
 class DashboardSerializer(serializers.ModelSerializer):
     """
     Serializer for the dashboard api.
     """
+    config = DashboardConfigField()
+
     class Meta:
-        model = models.Dashboard
+        model = Dashboard
         fields = ['id', 'name', 'owner', 'config']
