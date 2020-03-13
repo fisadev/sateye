3e8bd1999e7c582f8e83f80838a2251b5cca45f2
Juan Pedro Fisanotti <fisadev@gmail.com>
2020-03-13 19:57:53 -0300

    All the needed stuff to run in heroku

--- sateye/settings.py
+++ sateye/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 
 import os
 
+import django_heroku
+
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -141,3 +144,5 @@ LOGGING = {
         },
     },
 }
+
+django_heroku.settings(locals())
